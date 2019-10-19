b311adfc6a679e3194c1ff96435e85aeb3853b2c
Jishan Ansari <jishan010@gmail.com>
2019-10-19 19:05:27 +0530

    updated add note screen

--- app/src/main/java/com/mobility/mvvvmdesignpatterndemo/view/CreateNoteActivity.java
+++ app/src/main/java/com/mobility/mvvvmdesignpatterndemo/view/CreateNoteActivity.java
@@ -2,15 +2,38 @@ package com.mobility.mvvvmdesignpatterndemo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mobility.mvvvmdesignpatterndemo.R;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
+
+    private EditText titleEditText, descEditText;
+    private Button saveNoteButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+        titleEditText=findViewById(R.id.titleEditText);
+        descEditText=findViewById(R.id.descEditText);
+
+        saveNoteButton=findViewById(R.id.saveNoteButton);
+        saveNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(CreateNoteActivity.this,MainActivity.class);
+                intent.putExtra("title",titleEditText.getText().toString());
+                intent.putExtra("description",descEditText.getText().toString());
+                setResult(1,intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/mobility/mvvvmdesignpatterndemo/view/MainActivity.java
+++ app/src/main/java/com/mobility/mvvvmdesignpatterndemo/view/MainActivity.java
@@ -1,13 +1,19 @@
 package com.mobility.mvvvmdesignpatterndemo.view;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mobility.mvvvmdesignpatterndemo.R;
 import com.mobility.mvvvmdesignpatterndemo.model.Note;
 import com.mobility.mvvvmdesignpatterndemo.viewmodel.NoteViewModel;
@@ -16,20 +22,45 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
 
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NoteViewModel noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
-
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getNoteList().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-
+                Log.d(TAG, "onChanged: ");
             }
         });
+
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateNoteActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if(data != null) {
+                String title = data.getStringExtra("title");
+                String description = data.getStringExtra("description");
+                Note note = new Note(title, description);
+                noteViewModel.insertNote(note);
+            }
+        }
     }
 }
